a99b5861260013881a47cfc3ef17d2ca8ba3c884
xiaod <1778871523@qq.com>
2025-05-09 23:32:22 +0800

    feat: 新增定时任务管理功能并重构启动逻辑
    添加定时任务管理模块，支持添加、删除和检查任务。重构启动逻辑，将数据库、定时任务、日志和服务启动分离，提高代码可维护性。同时更新go.mod依赖，添加cron库支持定时任务功能。

--- go.mod
+++ go.mod
@@ -37,6 +37,7 @@ require (
 	github.com/gin-gonic/gin v1.10.0
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
+	github.com/robfig/cron/v3 v3.0.1
 	github.com/sagikazarmark/locafero v0.9.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
 	github.com/spf13/afero v1.14.0 // indirect
--- internal/app/gapi/start/run.go
+++ internal/app/gapi/start/run.go
@@ -1,21 +1,9 @@
 package start
 
-import (
-	"ginp-api/configs"
-	"ginp-api/internal/app/gapi/router"
-	"ginp-api/pkg/cfg"
-
-	"github.com/gin-gonic/gin"
-)
-
 // 程序入口
 func Run() {
-
-	//配置路径已定死在 cmd/gapi/configs.yaml ,要修改只能去改cfg包
-	//由于cionfigs包调用了init()初始化 因此使用cfg.initCfg函数可能会无效
-	port := cfg.GetString(configs.ServerPort)
-	r := gin.Default()
-	router.Register(r)
-	println("start server on port: " + port)
-	r.Run(":" + port)
+	startDB()
+	startTask()      //启动定时任务
+	startGinLogger() //启动日志
+	startGinServer() //启动http服务
 }
