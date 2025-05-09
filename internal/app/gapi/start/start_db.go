94c114ba9b5adfd8e61202530eb95b0e3e1d2c20
xiaod <1778871523@qq.com>
2025-05-10 00:21:14 +0800

    feat(db): 添加MySQL数据库初始化及自动迁移功能
    引入MySQL驱动及GORM库，实现数据库初始化及表结构自动迁移功能。修改了相关配置文件和依赖，确保数据库连接和迁移逻辑正确执行。

--- internal/app/gapi/start/start_db.go
+++ internal/app/gapi/start/start_db.go
@@ -1,5 +1,28 @@
 package start
 
-func startDB() {
+import (
+	"fmt"
+	"ginp-api/configs"
+	"ginp-api/internal/db/mysql"
+	"ginp-api/pkg/cfg"
+)
 
+func startDB() {
+	mysql.InitDb(
+		cfg.GetString(configs.ConfigKeyMysqlIp),
+		cfg.GetString(configs.ConfigKeyMysqlPort),
+		cfg.GetString(configs.ConfigKeyMysqlUser),
+		cfg.GetString(configs.ConfigKeyMysqlDb),
+		cfg.GetString(configs.ConfigKeyMysqlPwd),
+	)
+
+	//迁移表
+	if mysql.GetDbInstance() != nil {
+		//自动迁移表结构
+		err := mysql.GetDbInstance().AutoMigrate(EntityAutoMigrateList...)
+		if err != nil {
+			fmt.Println("迁移表结构失败" + err.Error())
+			panic(err)
+		}
+	}
 }
