f0f189a4200831f1c88d6664f9e33455c13475aa
xiaod <1778871523@qq.com>
2025-05-10 14:56:13 +0800

    2

--- cmd/gencode/desc/desc.go
+++ cmd/gencode/desc/desc.go
@@ -1,8 +1,9 @@
 package desc
 
-const InputGenType = `请回复数字选择生成类型：
-1. 创建实体
+const InputGenType = `1. 创建实体
 2. 根据实体生成CRUD代码
-3. 新增1个API接口`
+3. 新增1个API接口
+请回复数字选择生成类型：
+`
 
 const InputEntityName = `请输入实体名称：`
