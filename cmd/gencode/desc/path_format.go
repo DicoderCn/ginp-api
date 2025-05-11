563181e357e788bb61b95d5d372f0f7dd3954802
DicoderCn <dicoder@126.com>
2025-05-12 00:44:49 +0800

    refactor(路径生成): 统一使用NameToLine函数处理路径名称
    为了保持路径生成的一致性，将所有路径生成函数中的名称处理统一使用NameToLine函数，避免使用NameToAllSmall函数导致的不一致问题。

--- cmd/gencode/desc/path_format.go
+++ cmd/gencode/desc/path_format.go
@@ -7,34 +7,35 @@ import (
 
 // 组装 controller 路径
 func PathController(lineName string) string {
-	//baseName := gen.NameToLine(lineName)
-	baseName := gen.NameToAllSmall(lineName)
+	lineName = gen.NameToLine(lineName)
+	baseName := gen.NameToLine(lineName)
 	return filepath.Join(GetDirController(), "c"+baseName, baseName+".c.go")
 }
 
 // 组装 service 路径
 func PathService(lineName string) string {
-
-	//baseName := gen.NameToLine(lineName)
+	lineName = gen.NameToLine(lineName)
 	baseName := gen.NameToAllSmall(lineName)
-	return filepath.Join(GetDirService(), "s"+baseName, baseName+".s.go")
+	return filepath.Join(GetDirService(), "s"+baseName, lineName+".s.go")
 }
 
 // 组装 model 路径
 func PathModel(lineName string) string {
-	//baseName := gen.NameToLine(lineName)
+	lineName = gen.NameToLine(lineName)
 	baseName := gen.NameToAllSmall(lineName)
-	return filepath.Join(GetDirModel(), "m"+baseName, baseName+".m.go")
+	return filepath.Join(GetDirModel(), "m"+baseName, lineName+".m.go")
 }
 
 // 组装路由路径 router
 func PathRouter(lineName string) string {
-	baseName := gen.NameToLine(lineName)
-	return filepath.Join(GetDirRouter(), baseName+".r.go")
+	lineName = gen.NameToLine(lineName)
+	// baseName := gen.NameToAllSmall(lineName)
+	return filepath.Join(GetDirRouter(), lineName+".r.go")
 }
 
 // 组装实体路径 entity
 func PathEntity(lineName string) string {
-	baseName := gen.NameToLine(lineName)
-	return filepath.Join(GetDirEntidy(), baseName+".e.go")
+	lineName = gen.NameToLine(lineName)
+	// baseName := gen.NameToAllSmall(lineName)
+	return filepath.Join(GetDirEntidy(), lineName+".e.go")
 }
