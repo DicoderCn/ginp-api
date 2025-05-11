edad1b66e5b063f94a6eb7fc0a8fabbc05a3b9b7
DicoderCn <dicoder@126.com>
2025-05-11 15:53:35 +0800

    fix(router): 将根路径的HTTP请求类型从POST更改为GET
    更改根路径的HTTP请求类型以匹配预期的GET请求，避免404错误

--- internal/app/gapi/router/r.index.go
+++ internal/app/gapi/router/r.index.go
@@ -10,7 +10,7 @@ func InitRouter() {
 	ginp.RouterAppend(ginp.RouterItem{
 		Path:      "/",                                    //api路径
 		Handlers:  ginp.RegisterHandler(cindex.IndexView), //对应控制器
-		HttpType:  ginp.HttpPost,                          //http请求类型
+		HttpType:  ginp.HttpGet,                           //http请求类型
 		NeedLogin: false,                                  //是否需要登录
 	})
 }
