c2ba8256ba680c7abc33419f64beb51a1eaf9320
DicoderCn <dicoder@126.com>
2025-05-12 00:47:05 +0800

    2

--- cmd/gencode/desc/path_format.go
+++ cmd/gencode/desc/path_format.go
@@ -8,34 +8,34 @@ import (
 // 组装 controller 路径
 func PathController(lineName string) string {
 	lineName = gen.NameToLine(lineName)
-	baseName := gen.NameToLine(lineName)
-	return filepath.Join(GetDirController(), "c"+baseName, lineName+".c.go")
+	allSmallName := gen.NameToAllSmall(lineName)
+	return filepath.Join(GetDirController(), "c"+allSmallName, lineName+".c.go")
 }
 
 // 组装 service 路径
 func PathService(lineName string) string {
 	lineName = gen.NameToLine(lineName)
-	baseName := gen.NameToAllSmall(lineName)
-	return filepath.Join(GetDirService(), "s"+baseName, lineName+".s.go")
+	allSmallName := gen.NameToAllSmall(lineName)
+	return filepath.Join(GetDirService(), "s"+allSmallName, lineName+".s.go")
 }
 
 // 组装 model 路径
 func PathModel(lineName string) string {
 	lineName = gen.NameToLine(lineName)
-	baseName := gen.NameToAllSmall(lineName)
-	return filepath.Join(GetDirModel(), "m"+baseName, lineName+".m.go")
+	allSmallName := gen.NameToAllSmall(lineName)
+	return filepath.Join(GetDirModel(), "m"+allSmallName, lineName+".m.go")
 }
 
 // 组装路由路径 router
 func PathRouter(lineName string) string {
 	lineName = gen.NameToLine(lineName)
-	// baseName := gen.NameToAllSmall(lineName)
+	// allSmallName := gen.NameToAllSmall(lineName)
 	return filepath.Join(GetDirRouter(), lineName+".r.go")
 }
 
 // 组装实体路径 entity
 func PathEntity(lineName string) string {
 	lineName = gen.NameToLine(lineName)
-	// baseName := gen.NameToAllSmall(lineName)
+	// allSmallName := gen.NameToAllSmall(lineName)
 	return filepath.Join(GetDirEntidy(), lineName+".e.go")
 }
