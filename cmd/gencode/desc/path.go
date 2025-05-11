70522e7b95f9f32d8010e1a52fb774ab17cced5e
DicoderCn <dicoder@126.com>
2025-05-12 00:19:17 +0800

    refactor: 重构路径生成函数并使用NameToAllSmall方法
    将PathController、PathService和PathModel函数中的NameToLine方法替换为NameToAllSmall，以提高代码一致性和可读性。同时删除不再使用的user_group.e.go文件。

--- cmd/gencode/desc/path.go
+++ cmd/gencode/desc/path.go
@@ -58,20 +58,24 @@ func GetDirModel() string {
 
 // 组装 controller 路径
 func PathController(lineName string) string {
-	lineName = gen.NameToLine(lineName)
-	return filepath.Join(GetDirController(), "c"+lineName, lineName+".c.go")
+	//baseName := gen.NameToLine(lineName)
+	baseName := gen.NameToAllSmall(lineName)
+	return filepath.Join(GetDirController(), "c"+baseName, baseName+".c.go")
 }
 
 // 组装 service 路径
 func PathService(lineName string) string {
-	lineName = gen.NameToLine(lineName)
-	return filepath.Join(GetDirService(), "s"+lineName, lineName+".s.go")
+
+	//baseName := gen.NameToLine(lineName)
+	baseName := gen.NameToAllSmall(lineName)
+	return filepath.Join(GetDirService(), "s"+baseName, baseName+".s.go")
 }
 
 // 组装 model 路径
 func PathModel(lineName string) string {
-	lineName = gen.NameToLine(lineName)
-	return filepath.Join(GetDirModel(), "m"+lineName, lineName+".m.go")
+	//baseName := gen.NameToLine(lineName)
+	baseName := gen.NameToAllSmall(lineName)
+	return filepath.Join(GetDirModel(), "m"+baseName, baseName+".m.go")
 }
 
 // 模板路径 entity
