22a76b671790e06a211a1e9475c3bca7eb075307
1778871523@qq.com <1778871523@qq.com>
2025-05-12 10:46:10 +0800

    docs(desc): 更新生成代码提示信息
    更新了生成代码时的提示信息，增加了输入格式的说明，使其更清晰易懂

--- cmd/gencode/desc/desc.go
+++ cmd/gencode/desc/desc.go
@@ -2,6 +2,9 @@ package desc
 
 const InputGenType = `1. 创建实体并生成CRUD代码(Create A Entity And Generate CRUD Code)
 2. 生成实体字段常量(generate entity field constants)
-请回复数字选择生成类型(reply with a number to select):`
+请回复数字选择生成类型(reply with a number to select)
+input:`
 
-const Select1 = `请输入实体名称(Please enter the entity name):`
+const Select1 = `请输入实体名称,大驼峰命名,如UserGroup
+(Please enter the entity name, hump naming, such as UserGroup)
+input:`
