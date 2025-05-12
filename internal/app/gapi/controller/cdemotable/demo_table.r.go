211cda2137d5b526a536723487264f07e7779547
1778871523@qq.com <1778871523@qq.com>
2025-05-12 13:28:55 +0800

    refactor(router): 简化API路径常量命名
    移除API路径常量中的实体名称前缀，提高代码简洁性和可维护性

--- internal/app/gapi/controller/cdemotable/demo_table.r.go
+++ internal/app/gapi/controller/cdemotable/demo_table.r.go
@@ -8,11 +8,11 @@ import (
 )
 
 const (
-	ApiDemoTableCreate   = "/api/demo_table/create"
-	ApiDemoTableFindById = "/api/demo_table/findById"
-	ApiDemoTableSearch   = "/api/demo_table/search"
-	ApiDemoTableUpdate   = "/api/demo_table/update"
-	ApiDemoTableDelete   = "/api/demo_table/delete"
+	ApiCreate   = "/api/demo_table/create"
+	ApiFindById = "/api/demo_table/findById"
+	ApiSearch   = "/api/demo_table/search"
+	ApiUpdate   = "/api/demo_table/update"
+	ApiDelete   = "/api/demo_table/delete"
 )
 
 // this is router define file
@@ -20,7 +20,7 @@ func init() {
 
 	// Create
 	ginp.RouterAppend(ginp.RouterItem{
-		Path:           ApiDemoTableCreate,           //api路径
+		Path:           ApiCreate,                    //api路径
 		Handlers:       ginp.RegisterHandler(Create), //对应控制器
 		HttpType:       ginp.HttpPost,                //http请求类型
 		NeedLogin:      false,                        //是否需要登录
@@ -35,7 +35,7 @@ func init() {
 
 	// FindById
 	ginp.RouterAppend(ginp.RouterItem{
-		Path:           ApiDemoTableFindById,           //api路径
+		Path:           ApiFindById,                    //api路径
 		Handlers:       ginp.RegisterHandler(FindByID), //对应控制器
 		HttpType:       ginp.HttpPost,                  //http请求类型
 		NeedLogin:      false,                          //是否需要登录
@@ -50,7 +50,7 @@ func init() {
 
 	// 修改
 	ginp.RouterAppend(ginp.RouterItem{
-		Path:           ApiDemoTableUpdate,           //api路径
+		Path:           ApiUpdate,                    //api路径
 		Handlers:       ginp.RegisterHandler(Update), //对应控制器
 		HttpType:       ginp.HttpPost,                //http请求类型
 		NeedLogin:      true,                         //是否需要登录
@@ -65,7 +65,7 @@ func init() {
 
 	// 删除
 	ginp.RouterAppend(ginp.RouterItem{
-		Path:           ApiDemoTableDelete,           //api路径
+		Path:           ApiDelete,                    //api路径
 		Handlers:       ginp.RegisterHandler(Delete), //对应控制器
 		HttpType:       ginp.HttpPost,                //http请求类型
 		NeedLogin:      true,                         //是否需要登录
@@ -80,7 +80,7 @@ func init() {
 
 	// search 搜索
 	ginp.RouterAppend(ginp.RouterItem{
-		Path:           ApiDemoTableSearch,           //api路径
+		Path:           ApiSearch,                    //api路径
 		Handlers:       ginp.RegisterHandler(Search), //对应控制器
 		HttpType:       ginp.HttpPost,                //http请求类型
 		NeedLogin:      true,                         //是否需要登录
