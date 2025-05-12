02916cb4f7372518c5590e8cb90661a781669eba
1778871523@qq.com <1778871523@qq.com>
2025-05-12 13:30:07 +0800

    docs(desc): 更新提示信息中的输入描述
    更新了`desc.go`文件中的提示信息，将`input:`改为更明确的`input code number:`和`input entity name:`，以提高用户输入的清晰度

--- cmd/gencode/desc/desc.go
+++ cmd/gencode/desc/desc.go
@@ -3,8 +3,8 @@ package desc
 const InputGenType = `1. 创建实体并生成CRUD代码(Create A Entity And Generate CRUD Code)
 2. 生成实体字段常量(generate entity field constants)
 请回复数字选择生成类型(reply with a number to select)
-input:`
+input code number:`
 
 const Select1 = `请输入实体名称,大驼峰命名,如UserGroup
 (Please enter the entity name, hump naming, such as UserGroup)
-input:`
+input entity name:`
